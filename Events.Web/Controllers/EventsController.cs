3129b23e9fd29c6c29fb4fba9ce2fa9d544237d4
diwanshu <diwanshusharma23@gmail.com>
2021-01-08 13:03:05 +0530

    Add project files.

---
+++ Events.Web/Controllers/EventsController.cs
@@ -0,0 +1,59 @@
+﻿using Events.Data;
+using Events.Web.Extensions;
+using Events.Web.Models;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Events.Web.Controllers
+{
+    public class EventsController : BaseController
+    {
+        // GET: Event
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(EventInputModel model)
+        {
+            if(model != null && this.ModelState.IsValid)
+            {
+                var e = new Event()
+                {
+                    AuthorId = this.User.Identity.GetUserId(),
+                    Title = model.Title,
+                    StartDateTime = model.StartdateTime,
+                    Duration = model.Duration,
+                    Description = model.Description,
+                    Location = model.Location,
+                    IsPublic = model.IsPublic
+                };
+
+                this.db.Events.Add(e);
+                this.db.SaveChanges();
+
+                // Display message
+                this.AddNotification("Event created.", NotificationType.INFO);
+                return this.RedirectToAction("My");
+            }
+
+            return this.View(model);
+        }
+
+        public ActionResult My()
+        {
+            return View();
+        }
+    }
+}
---
+++ Events.Web/Models/EventInputModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Events.Web.Models
+{
+    public class EventInputModel
+    {
+        [Required(ErrorMessage ="Event title is Required.")]
+        [StringLength(200, ErrorMessage ="the {0} must be betweeen {2} and {1} characters long.", MinimumLength =1)]
+        [Display(Name ="Title")]
+        public string Title { get; set; }
+
+        [DataType(DataType.DateTime)]
+        [Display(Name ="Date and Time *")]
+        public DateTime StartdateTime { get; set; }
+
+        public TimeSpan? Duration { get; set; }
+        public string Description { get; set; }
+
+        [MaxLength(200)]
+        public string Location { get; set; }
+
+        [Display(Name ="Is Public ?")]
+        public bool IsPublic { get; set; }
+    }
+}
