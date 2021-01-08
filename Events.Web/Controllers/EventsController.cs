1e3cf1dd4ec207958899a1d4487726c2b277df00
diwanshu <diwanshusharma23@gmail.com>
2021-01-08 18:05:03 +0530

    till 16 done

--- Events.Web/Controllers/EventsController.cs
+++ Events.Web/Controllers/EventsController.cs
@@ -10,6 +10,7 @@ using System.Web.Mvc;
 
 namespace Events.Web.Controllers
 {
+    [Authorize]
     public class EventsController : BaseController
     {
         // GET: Event
@@ -53,7 +54,85 @@ namespace Events.Web.Controllers
 
         public ActionResult My()
         {
-            return View();
+            string currentuserId = this.User.Identity.GetUserId();
+            var events = this.db.Events
+                .Where(e => e.AuthorId == currentuserId)
+                .OrderBy(e => e.StartDateTime)
+                .Select(EventViewModel.ViewModel);
+
+            var upcomingEvents = events.Where(e => e.StartDateTime > DateTime.Now);
+            var passedEvents = events.Where(e => e.StartDateTime <= DateTime.Now);
+
+            return View(new UpcomingPassedEventsViewModel()
+            {
+                UpComingEvents = upcomingEvents,
+                PassedEvents = passedEvents
+            });
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            var eventToEdit = this.LoadEvent(id);
+            if(eventToEdit == null)
+            {
+                this.AddNotification("Cannot edit event # " + id,
+                    NotificationType.ERROR);
+
+                return this.RedirectToAction("My");
+            }
+
+            // var model = this.db.Events.Find(id);
+            // var model = EventInputModel.CreateFromEvent(eventToEdit);
+            
+            var model = from e in this.db.Events
+            where e.Id == id
+            select new EventInputModel()
+            {
+                    Title = e.Title,
+                    StartdateTime = e.StartDateTime,
+                    Duration = e.Duration,
+                    Description = e.Description,
+                    Location = e.Location,
+                    IsPublic = e.IsPublic
+            };
+            return this.View(model);
+        }
+
+        private Event LoadEvent(int id)
+        {
+            var currentuserId = this.User.Identity.GetUserId();
+            var isAdmin = this.IsAdmin();
+            var eventToEdit = this.db.Events
+                .Where(e => e.Id == id)
+                .FirstOrDefault(e => e.AuthorId == currentuserId || isAdmin);
+            return eventToEdit;
+        }
+
+        public ActionResult Edit(int id, EventInputModel model)
+        {
+            var eventToEdit = this.LoadEvent(id);
+            if(eventToEdit == null)
+            {
+                this.AddNotification("Can not edit events # " + id, NotificationType.ERROR);
+                return this.RedirectToAction("My");
+            }
+
+            if(model != null && this.ModelState.IsValid)
+            {
+                eventToEdit.Title = model.Title;
+                eventToEdit.StartDateTime = model.StartdateTime;
+                eventToEdit.Duration = model.Duration;
+                eventToEdit.Description = model.Description;
+                eventToEdit.Location = model.Location;
+                eventToEdit.IsPublic = model.IsPublic;
+
+                this.db.SaveChanges();
+                this.AddNotification("Event edit.", NotificationType.INFO);
+                return this.RedirectToAction("My");
+            }
+
+            return this.View(model);
         }
     }
 }
